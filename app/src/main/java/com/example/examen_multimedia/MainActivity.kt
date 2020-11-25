2861fb947fe3cb6cb701e2e2368d60b0ce96b8e5
AlumnoM <aitorambite@protonmail.com>
2020-11-25 14:03:56 +0100

    commit

--- app/src/main/java/com/example/examen_multimedia/MainActivity.kt
+++ app/src/main/java/com/example/examen_multimedia/MainActivity.kt
@@ -12,23 +12,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val second_activity = Intent(this,SecondActivity::class.java)
-        et_activity_1.addTextChangedListener(object : TextWatcher{
+
+        val second_activity = Intent(this, SecondActivity::class.java)
+
+        et_activity_1.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
             }
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
+
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 var lista = et_activity_1.text.split(" ")
-                if(lista.size >= 4){
+                if (lista.size >= 4) {
                     bt_lanzar_second.isEnabled = true;
                 }
             }
 
         })
-        bt_lanzar_second.setOnClickListener{
-            second_activity.putExtra(SecondActivity.TAG_SECOND_1,et_activity_1.text.toString())
+
+        bt_lanzar_second.setOnClickListener {
+            second_activity.putExtra(SecondActivity.TAG_SECOND_1, et_activity_1.text.toString())
             startActivity(second_activity)
         }
     }
--- app/src/main/java/com/example/examen_multimedia/SecondActivity.kt
+++ app/src/main/java/com/example/examen_multimedia/SecondActivity.kt
@@ -1,13 +1,16 @@
 package com.example.examen_multimedia
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.second_activity.*
 
 class SecondActivity : AppCompatActivity() {
-    companion object{
+    companion object {
         const val TAG_SECOND_1 = "TAG_SECOND_1"
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.second_activity)
@@ -18,35 +21,41 @@ class SecondActivity : AppCompatActivity() {
 
         textoObtenido?.run {
             conversionLista = textoObtenido.split(" ").toMutableList()
+            conversionLista?.forEach {
+                textoMostrar += it + "\n"
+            }
+            tv_second.text = textoMostrar
+        }
 
-            conversionLista?.forEach{
+        bt_second_1.setOnClickListener {
+            textoMostrar = ""
+            conversionLista?.forEach {
+                textoMostrar += it + " " + it.length + "\n"
+            }
+            tv_second.text = textoMostrar
+        }
+        bt_second_2.setOnClickListener {
+            textoMostrar = ""
+            conversionLista.sorted()?.forEach {
                 textoMostrar += it+"\n"
             }
             tv_second.text = textoMostrar
         }
 
-        bt_second_1.setOnClickListener{
+        bt_second_3.setOnClickListener {
             textoMostrar = ""
-            conversionLista?.forEach{
-                textoMostrar += it+" "+it.length+"\n"
-            }
-            tv_second.text = textoMostrar
-        }
-        bt_second_2.setOnClickListener{
+            if (!et_second_number.text.isNullOrEmpty()) {
+                var filtro = conversionLista.asSequence()
+                    .filter { it.length > et_second_number.text.toString().toInt() }
 
-        }
-
-        bt_second_3.setOnClickListener{
-            var filtro = conversionLista.asSequence().filter{ it.length > et_second_number.text.toString().toInt()}
-            textoMostrar = ""
-            et_second_number?.run{
                 filtro?.forEach {
-                    textoMostrar += it+"\n"
+                    textoMostrar += it + "\n"
                 }
                 tv_second.text = textoMostrar.toString()
+            }else{
+                Toast.makeText(this,"parece que no has metido ningun numero" +
+                        " querias pillarme eee....",Toast.LENGTH_LONG).show()
             }
         }
     }
-
-
 }
