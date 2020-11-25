3234b0b5ebf7c68731ee4340c6dfacb8711d5162
AlumnoM <aitorambite@protonmail.com>
2020-11-25 13:39:47 +0100

    commit

--- app/src/main/java/com/example/examen_multimedia/SecondActivity.kt
+++ app/src/main/java/com/example/examen_multimedia/SecondActivity.kt
@@ -1,9 +1,6 @@
 package com.example.examen_multimedia
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.second_activity.*
 
@@ -14,34 +11,19 @@ class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.second_activity)
-        var texto = intent.getStringExtra(TAG_SECOND_1)
+        var textoObtenido = intent.getStringExtra(TAG_SECOND_1)
         var conversionLista = mutableListOf("")
+
         var textoMostrar = ""
 
-        texto?.run {
-            conversionLista = texto.split(" ").toMutableList()
+        textoObtenido?.run {
+            conversionLista = textoObtenido.split(" ").toMutableList()
+
             conversionLista?.forEach{
                 textoMostrar += it+"\n"
             }
             tv_second.text = textoMostrar
         }
-//        et_second_number.addTextChangedListener(object  : TextWatcher{
-//            override fun afterTextChanged(s: Editable?) {
-//                TODO("Not yet implemented")
-//            }
-//
-//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-//                TODO("Not yet implemented")
-//            }
-//
-//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-//                var eleccion = et_second_number.text.toString().toInt()
-//                if(eleccion > 0){
-//                    et
-//                }
-//            }
-//
-//        })
 
         bt_second_1.setOnClickListener{
             textoMostrar = ""
@@ -53,6 +35,18 @@ class SecondActivity : AppCompatActivity() {
         bt_second_2.setOnClickListener{
 
         }
+
+        bt_second_3.setOnClickListener{
+            var filtro = conversionLista.asSequence().filter{ it.length > et_second_number.text.toString().toInt()}
+            textoMostrar = ""
+            et_second_number?.run{
+                filtro?.forEach {
+                    textoMostrar += it+"\n"
+                }
+                tv_second.text = textoMostrar.toString()
+            }
+        }
     }
+
 
 }
